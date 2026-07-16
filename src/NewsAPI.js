e7124a6e9468c526891b7f2aa0fdae0d8a116931
vforvishal12 <52970035+vforvishal12@users.noreply.github.com>
2026-07-16 10:13:54 +0530

    Use CORS proxy for NewsAPI fetch requests
    Updated fetch URL to use CORS proxy for API requests.

--- src/NewsAPI.js
+++ src/NewsAPI.js
@@ -13,10 +13,15 @@ class NewsAPI extends Component {
     };
 
     componentDidMount() {
-        fetch('https://newsapi.org/v2/everything?q=india&apiKey=34300e3dec4f49f8bc1b82e638dd3f4b')
+        // Using CORS proxy to bypass the CORS restriction
+        const apiKey = '34300e3dec4f49f8bc1b82e638dd3f4b';
+        const newsApiUrl = `https://newsapi.org/v2/everything?q=india&apiKey=${apiKey}`;
+        const corsProxyUrl = `https://cors-anywhere.herokuapp.com/${newsApiUrl}`;
+
+        fetch(corsProxyUrl)
             .then(res => res.json())
             .then((data) => {
-                console.log(data); // Debug: check what data you're getting
+                console.log(data);
                 this.setState({ articles: data.articles || [] })
             })
             .catch(err => {
